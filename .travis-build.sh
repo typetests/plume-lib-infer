486aef7b3db39df593d83d8d971c924d9e9cc688
Michael Ernst <mernst@cs.washington.edu>
2016-07-05 06:40:49 -0700

    Retain import statements in comments, to reduce diff size

--- .travis-build.sh
+++ .travis-build.sh
@@ -15,11 +15,16 @@ export CHECKERFRAMEWORK=$ROOT/checker-framework
 
 cd $ROOT/plume-lib
 
-# Remove all annotations in comments
-(cd java && find \( -name '*.java' -o -name '*.jpp' \) -exec perl -i -00pe's/\/\*@[A-Z][^*]*?[a-z)]\*\/[ \n]*//sg; s/\/\*>>>.*?\*\/\n*//sg;' {} +)
+# Remove annotations in comments
+(cd java && find \( -name '*.java' -o -name '*.jpp' \) -exec perl -i -00pe's/\/\*@[A-Z][^*]*?[a-z)]\*\/[ \n]*//sg;' {} +)
+## Retain import statements in comments, to reduce the size of the diffs
+# # Remove import statements in comments
+# (cd java && find \( -name '*.java' -o -name '*.jpp' \) -exec perl -i -00pe's/\/\*>>>.*?\*\/\n*//sg;' {} +)
 
 make jar
 $CHECKERFRAMEWORK/checker/bin/infer-and-annotate.sh \
     "nullness,interning,formatter,signature,lock" java/plume.jar:java/lib/junit-4.12.jar \
     -AprintErrorStack \
     `find java/src/plume/ -name "*.java"`
+
+git diff
