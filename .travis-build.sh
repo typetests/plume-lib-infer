a053917ad272e42afb259b1b5f5aaf2f209481f0
Michael Ernst <mernst@cs.washington.edu>
2017-08-13 08:19:29 -0700

    Download the JDK rather than rebuilding it

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,7 +7,7 @@ set -e
 ## Build Checker Framework
 (cd $ROOT && git clone https://github.com/typetools/checker-framework.git)
 # This also builds annotation-tools and jsr308-langtools
-(cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh)
+(cd $ROOT/checker-framework/ && ./.travis-build-without-test.sh downloadjdk)
 export CHECKERFRAMEWORK=$ROOT/checker-framework
 
 ## Obtain plume-lib
